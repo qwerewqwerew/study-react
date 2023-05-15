a2b0adcc9772fd7a2019e3a134df9f555bf680e7
qwerewqwerew <qwerew0@gmail.com>
2023-05-15 14:01:36 +0900

    composition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Expenses from "./components/Expenses";
 function App() {
+  const expense = [
+    { id: 'e1', title: '화장지', amount: 9100, date: new Date(2022, 7, 14) },
+    { id: 'e2', title: '모니터', amount: 129100, date: new Date(2022, 12, 14) },
+    { id: 'e3', title: '자동차보험', amount: 294000, date: new Date(2023, 3, 14) },
+    { id: 'e4', title: '밥상', amount: 4500, date: new Date(2023, 5, 5) }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>가계부</h1>
+      <Expenses items={expense}/>
     </div>
   );
 }
